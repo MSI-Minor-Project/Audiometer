06297e35571d6ffb131e8d13042a428dafbc7b83
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-10-25 17:02:11 +0530

    Added Login and Home screens to the screens folder

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,5 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+    return <Text>This is the home screen</Text>
+}
---
+++ screens/LoginScreen.tsx
@@ -0,0 +1,8 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function LoginScreen() {
+    return <SafeAreaView>
+        <Text>This is the login screen</Text>
+    </SafeAreaView>
+}
