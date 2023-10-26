6819a1dcd27c3670c310ee4615739be257f95d2c
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-10-26 15:55:04 +0530

    Added login screen

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,8 +1,72 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Avatar, Button, Card, Text, TextInput } from 'react-native-paper';
+import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 export default function LoginScreen() {
+
+    const [text, setText] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [secureTextEntry, setSecureTextEntry] = useState(true);
+    const navigation = useNavigation();
+
     return <SafeAreaView>
-        <Text>This is the login screen</Text>
+        <View className='flex flex-col justify-center items-center h-full'>
+            <Card mode='elevated' style={{
+                padding: 10,
+            }}>
+                <Card.Content style={{
+                    rowGap: 7,
+                }}>
+                    <TextInput
+                        mode="outlined"
+                        label="Email"
+                        value={text}
+                        onChangeText={text => setText(text)}
+                    />
+                    <TextInput
+                        mode="outlined"
+                        label="Enter Password"
+                        value={password}
+                        onChangeText={text => setPassword(text)}
+                        secureTextEntry={secureTextEntry}
+                        caretHidden={true}
+                        right={
+                            <TextInput.Icon icon="eye" onPress={() => {
+                                setSecureTextEntry(!secureTextEntry);
+                            }} />
+                        }
+                    />
+                </Card.Content>
+                <Card.Actions style={{
+                    width: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                }}>
+                    <TouchableOpacity style={{
+                        width: "100%",
+                    }}>
+                        <Button mode="contained-tonal">
+                            Login
+                        </Button>
+                    </TouchableOpacity>
+                </Card.Actions>
+                <Card.Content style={{
+                    width: "100%",
+                    display: "flex",
+                    flexDirection: "row",
+                    rowGap: 7,
+                    justifyContent: "center",
+                }}>
+                    <Text variant='bodyLarge'>Don't have an account? </Text>
+                    <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+                        <Text variant='bodyLarge' style={{ color: "purple" }}>
+                            Register here
+                        </Text>
+                    </TouchableOpacity>
+                </Card.Content>
+            </Card>
+        </View>
     </SafeAreaView>
 }
