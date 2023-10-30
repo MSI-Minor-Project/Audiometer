ce079c0e6436e62c048bc8153a6f0ab2ca150273
Kaartik Shukla <kaartikshukla7120@gmail.com>
2023-10-30 18:55:06 +0530

    Added navigation to the plus icon to navigate to test modal

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,8 +2,16 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Ionicons, AntDesign } from '@expo/vector-icons';
 import { Divider, Button } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
+import { RootStack } from '../App';
+import { StackNavigationProp } from '@react-navigation/stack';
+
+type StackProps = StackNavigationProp<RootStack, "Test">;
 
 export default function HomeScreen() {
+
+    const navigation = useNavigation<StackProps>();
+
     return <View className='flex flex-col items-center justify-between p-7 h-full'>
         <View className='flex flex-col items-center gap-y-1'>
             <Ionicons name="md-information-circle" size={42} color="black" />
@@ -17,7 +25,9 @@ export default function HomeScreen() {
             backgroundColor: 'black',
         }} />
         <View className='flex flex-col items-center gap-y-2'>
-            <TouchableOpacity onPress={() => { }} >
+            <TouchableOpacity onPress={() => {
+                navigation.navigate("Test");
+            }} >
                 <AntDesign name="pluscircleo" size={150} color="black" />
             </TouchableOpacity>
             <Text className='font-bold text-lg'>Press to start the test</Text>
